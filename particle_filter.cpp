e0aed91b57bf32876b79309a567b996449edce82
murilo25 <42446108+murilo25@users.noreply.github.com>
2020-07-25 08:55:32 -0500

    Implement ParticleFilter::updateWeights()

--- particle_filter.cpp
+++ particle_filter.cpp
@@ -24,7 +24,7 @@ using std::string;
 using std::vector;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
-    /**
+    /*
     * Set the number of particles. 
     * Initialize all particles to first position (based on estimates of x, y, theta and their uncertainties
     * from GPS and IMU) and all weights to 1. 
@@ -65,7 +65,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 void ParticleFilter::prediction(double delta_t, double std_pos[], 
                                 double velocity, double yaw_rate) {
 
-    /**
+    /*
     * Predict particle location based on noisy position measurement and noiseless control inputs (velocity & yaw_rate)
     * (position, velocity and yaw_rate)
     */
@@ -97,7 +97,7 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
 
 void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted, 
                                      vector<LandmarkObs>& observations) {
-  /**
+  /*
    * TODO: Find the predicted measurement that is closest to each 
    *   observed measurement and assign the observed measurement to this 
    *   particular landmark.
@@ -108,23 +108,91 @@ void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted,
 
 }
 
-void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
-                                   const vector<LandmarkObs> &observations, 
-                                   const Map &map_landmarks) {
-  /**
-   * TODO: Update the weights of each particle using a mult-variate Gaussian 
-   *   distribution. You can read more about this distribution here: 
-   *   https://en.wikipedia.org/wiki/Multivariate_normal_distribution
-   * NOTE: The observations are given in the VEHICLE'S coordinate system. 
-   *   Your particles are located according to the MAP'S coordinate system. 
-   *   You will need to transform between the two systems. Keep in mind that
-   *   this transformation requires both rotation AND translation (but no scaling).
-   *   The following is a good resource for the theory:
-   *   https://www.willamette.edu/~gorr/classes/GeneralGraphics/Transforms/transforms2d.htm
-   *   and the following is a good resource for the actual equation to implement
-   *   (look at equation 3.33) http://planning.cs.uiuc.edu/node99.html
+void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], const vector<LandmarkObs> &observations, const Map &map_landmarks) {
+
+   /*
+   * Update the weights of each particle using a mult-variate Gaussian distribution.
+   * Observations are given in vehicle frame, so it requires transformation to map frame.
+   * Once transformation is performed, each observation is assigned to a landmark.
+   * In the multi-variate Gaussian, x,y are observations in map coordinates and mu_x,mu_y are the coordinates of nearest landmarks.
    */
 
+    vector<LandmarkObs> transformedObs; // vector with transformed observations
+    LandmarkObs transformedObs_i;   // to help constructing transformedObs vector
+
+    // for every particles, transform observations from vehicle to map coordinates
+    for (int p = 0; p < num_particles; p++)
+    {
+        double x_part = particles[p].x;
+        double y_part = particles[p].y;
+        double theta_part = particles[p].theta;
+
+        for (int i = 0; i < observations.size(); i++)
+        {
+            double x_obs = observations[i].x;
+            double y_obs = observations[i].y;
+
+            // transform to map x coordinate
+            double x_map;
+            x_map = x_part + (cos(theta_part) * x_obs) - (sin(theta_part) * y_obs);
+
+            // transform to map y coordinate
+            double y_map;
+            y_map = y_part + (sin(theta_part) * x_obs) + (cos(theta_part) * y_obs);
+            
+            transformedObs_i.id_i = i;
+            transformedObs_i.x = x_map;
+            transformedObs_i.y = y_map;
+
+            transformedObs.push_back(transformedObs_i)
+        }
+    }
+
+    double distance;
+    double nearest_neighbor_dist = sensor_range;
+    double nearest_neighbor_id;
+
+    // associate observed landmarks to map landmarks
+    for (int t = 0; t < transformedObs.size(); t++) // for each observation
+    {
+        for (int i = 0; i < map_landmarks.size(); i++)  // for each landmark
+        {
+            // compute distance between i-th landmark(map) and t-th observation
+            distance = dist(map_landmarks[i].x_f, map_landmarks[i].y_f, transformedObs[t].x, transformedObs[t].y);
+
+            //if (distance < sensor_range)    // ignore landmarks that are not within sensor range (?)
+            //{
+                if (distance < nearest_neighbor_dist)
+                {
+                    nearest_neighbor_dist = distance;
+                    nearest_neighbor_id = i;
+                }
+            //}
+        }
+        transformedObs[t].id = i;   // assign matching landmark id to t-th observation
+    }
+
+    double mu_x;
+    double mu_y;
+    double sig_x = std_landmark[0];
+    double sig_y = std_landmark[1];
+    double w;
+    for (int p = 0; p < num_particles; p++) // each particle
+    {
+        w = 1;
+        for (int t = 0; t < transformedObs.size(); t++) // each observation
+        {
+            mu_x = map_landmarks[transformedObs[t].id].x;
+            mu_y = map_landmarks[transformedObs[t].id].y;
+
+            //if (dist(particles[p].x, particles[p].y, transformedObs[t].x, transformedObs[t].y) < sensor_range) // redundant (?)
+            //{
+
+                w *= (1 / (2 * M_PI * sig_x * sig_y)) * exp(-(pow((transformedObs[t].x - mu_x), 2) / (2 * sig_x * sig_x) + pow((transformedObs[t].y - mu_y), 2) / (2 * sig_y * sig_y)));
+            //}
+        }
+        particles[p].weight = w;
+    }
 }
 
 void ParticleFilter::resample() {
